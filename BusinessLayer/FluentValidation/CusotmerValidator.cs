d0c8f47caeae1c1698c52449698301692fefc970
Berkay Kaya <80210343+imberkaykaya@users.noreply.github.com>
2024-05-20 11:20:07 +0300

    Add project files.

---
+++ BusinessLayer/FluentValidation/CusotmerValidator.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.FluentValidation
+{
+    public class CusotmerValidator : AbstractValidator<Customer>
+    {
+        public CusotmerValidator()
+        {
+            RuleFor(x => x.City).NotEmpty().WithMessage("Şehir bilgisi boş geçilemez");
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Ad boş geçilemez");
+        }
+    }
+}
---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=SARILARNB67;database=DemoProductCore;Integrated Security=True;TrustServerCertificate=True");
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
