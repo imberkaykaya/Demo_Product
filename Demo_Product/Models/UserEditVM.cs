8967a1acbd794c80ae1b71c29b7b5b3ee72eb6d8
Berkay Kaya <80210343+imberkaykaya@users.noreply.github.com>
2024-05-23 17:22:20 +0300

    last updated. I'll do login operation and finished this project...

---
+++ Demo_Product/Models/UserEditVM.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Demo_Product.Models
+{
+    public class UserEditVM
+    {
+        [Required(ErrorMessage = "Lütfen İsim Giriniz")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Soyisim Giriniz")]
+        public string SurName { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Mail Adresi Giriniz")]
+        public string Mail { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Cinsiyet Seçiniz")]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Şifre Giriniz")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Şifreyi Tekrar Giriniz")]
+        [Compare("Password", ErrorMessage = "Lütfen şifrelerin eşleştiğinden emin olun")]
+        public string ConfirimPassword { get; set; }
+    }
+}
